c0dc108b943a8a66b5bf5662fc4246fd137d5548
David <davidgraniertls@gmail.com>
2021-08-09 14:26:01 +0200

    Remplacement "Thing" par "Sauce"

--- models/stuff.js
+++ models/stuff.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema({
+const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
@@ -12,7 +12,7 @@ const thingSchema = mongoose.Schema({
    
 });
 
-module.exports = mongoose.model('Thing', thingSchema); 
+module.exports = mongoose.model('Sauce', sauceSchema);
 
 
 
